4f5d70c7bab937d04ce42695d6060a460976bb35
heinzwittenbrink <heinz.wittenbrink@gmail.com>
2019-09-07 11:33:36 +0200

    Added neǌs and rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -13,3 +13,8 @@ module.exports = function(eleventyConfig) {
     }
   };
 };
+
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+};
